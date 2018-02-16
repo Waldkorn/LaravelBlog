b9c7584dd75cfee07996f5e71502c7f71b4d30c8
Waldkorn <32877557+Waldkorn@users.noreply.github.com>
2018-02-16 10:18:32 +0100

    getting heroku to work

--- database/migrations/2018_02_14_153959_update_posts_table.php
+++ database/migrations/2018_02_14_153959_update_posts_table.php
@@ -15,7 +15,7 @@ class UpdatePostsTable extends Migration
     {
         //
         Schema::table('posts', function (Blueprint $table) {
-            $table->integer('category_id')->unsigned();
+            $table->integer('category_id')->unsigned()->nullable();
         });
     }
 
