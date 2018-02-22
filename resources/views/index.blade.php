79581f948e9e44113f298eb1d2eff8322ecd1db8
rkolen <rkolen2007@gmail.com>
2018-02-22 11:19:48 +0100

    [W6-013] blogger can use pictures as backgroundheader

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -56,11 +56,18 @@
 
               @if ($user->id == Auth::id())
 
-                  <form method="POST" action="/posts/blog/{{ $user->id }}/changeBlogName">
+                  <form method="POST" action="/posts/blog/{{ $user->id }}/blogEdit">
                     {{ csrf_field() }}
-                    <div class="form-group">
-                      <input class="form-control" name="blogname" value="{{ $user->blog_name }}" placeholder="type a blogname..." required>
-                    </div>
+                    
+                      <div class="form-group">
+                        <div class="container">
+                          <div class="row">
+                            <input class="form-control" name="blogpicture" value="{{ $user->blog_header_picture }}" placeholder="type an url..." required>
+                            <input class="form-control" name="blogname" value="{{ $user->blog_name }}" placeholder="type a blogname..." required>
+                            <input type="submit" class="btn btn-warning ml-auto" value="Edit Blog">
+                          </div>
+                        </div>
+                      </div>
                   </form>
 
                 @include('layouts.errors')
