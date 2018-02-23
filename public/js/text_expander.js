2c8b67262b410d0a045bf5b4b090adc5b26ae555
Waldkorn <32877557+Waldkorn@users.noreply.github.com>
2018-02-23 14:53:00 +0100

    minor bugfix

--- public/js/text_expander.js
+++ public/js/text_expander.js
@@ -11,6 +11,12 @@ init_instance_callback : function(editor) {
       mceEditor.dom.remove(bookmark.id + "_start");
     });
   },
+  plugins: [
+    "advlist autolink lists link image charmap print preview anchor",
+    "searchreplace visualblocks code fullscreen",
+    "insertdatetime media table contextmenu paste"
+  ],
+  toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
   height: "380",
   resize: false
 });
