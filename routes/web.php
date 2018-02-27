68fbe0c251f26b81dd57c3dcfacbae5e8c60cd4d
Riccardo Parrello <riccardoparrello@outlook.com>
2018-02-27 16:42:14 +0100

    [W7-009]
    The environment values are now used. Also support both mysql and pgsql.

--- routes/web.php
+++ routes/web.php
@@ -92,8 +92,25 @@ Route::get('/information', 'InformationController@index');
 //////////////////////////////////
 
 Route::get('/sqldump', function(){
-  //get password from env, linux and pgsql
-  exec('C:\xampp\mysql\bin\mysqldump laravel3 -uroot -pcodegorilla > database_backup.sql');
+  
+  $command;
+  $dbConnection = env('DB_CONNECTION');
+  $dbName = env('DB_DATABASE');
+  $dbHost = env('DB_HOST');
+  $dbPort = env('DB_PORT');
+  $dbUsername = env('DB_USERNAME');
+  $dbPassword = env('DB_PASSWORD');
+
+  switch ($dbConnection) {
+    case "mysql":
+        $command = "mysqldump $dbName -h$dbHost -P$dbPort -u$dbUsername -p$dbPassword > database_backup.sql";
+        break;
+    case "pgsql":
+        $command = "PGPASSWORD='$dbPassword' pg_dump -h $dbHost -p $dbPort -U $dbUsername $dbName > database_backup.sql";
+        break;
+  }
+
+  exec($command);
   return response()->download('database_backup.sql')->deleteFileAfterSend(true);
 
 });
