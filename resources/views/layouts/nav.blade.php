9e14ebe030451296570585a6398303bb0b693d53
rkolen <rkolen2007@gmail.com>
2018-03-08 13:57:27 +0100

    [W07-F05] made website more usable on mobilephones

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,35 +1,49 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse navbar-nav" id="navbarNavAltMarkup">
-    <a class="nav-item nav-link active" href='/'>Home <span class="sr-only">(current)</span></a>
-    <a class="nav-item nav-link" href="/information">{{ __('messages.information') }}</a>
-    @if (Auth::check())
-      <a class="nav-item nav-link" href="/posts/create">{{ __('messages.createPost') }}</a>
-      <div class="nav-item nav-link active ml-auto">
-        <span>
-          <div class="row">
-          @include('layouts.language')
-          <span class="nav-item nav-link active">
-            {{ __('messages.welcome') }} <a href="/profile">{{ Auth::user()->name }}</a>(<a class="nav-link d-inline" href="/logout" style="padding-left:0px; padding-right:0px;">Logout</a>)
-          </span>
-          </div>
-        </span>
-      </div>
-    @else
-    <span class="ml-auto">
-      <div class="row">
+  <div class="container">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <ul class="collapse navbar-collapse navbar-nav" id="navbarNavAltMarkup">
+      <li class="nav-item active">
+        <a class="nav-link" href='/'>Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"  style="padding-left:20px;" href="/information">{{ __('messages.information') }}</a>
+      </li>
+      <li class="nav-item">
         @include('layouts.language')
-        <a class="nav-item nav-link ml-auto" href="/login">Log in</a>
-        <a class="nav-item nav-link ml-auto" href="/register">{{ __('messages.register') }}</a>
-      </div>
-    </span>
+      </li>
+      @if (Auth::check())
+   
+      <li class="nav-item">
+        <a class="nav-link" href="/posts/create">{{ __('messages.createPost') }}</a>
+      </li>
+   
     @endif
-  </div>
-
+  </ul>
+    @if (Auth::check())
+    <ul class="nav navbar-nav flex-grow justify-content-end" style="padding-left:100px;">
+      
+        <div class="row">
+        
+        <li class="nav-item active" >
+          {{ __('messages.welcome') }} <a href="/profile">{{ Auth::user()->name }}</a>
+        </li>
+        <li class="nav-item">
+          (<a class="nav-link d-inline" href="/logout" style="padding-left:0px; padding-right:0px;">Logout</a>)
+        </li>
+        </div>
+    </ul>
+    @else
+    <ul class="nav navbar-nav flex-grow justify-content-end" style="padding-left:150px;">
+     <li class="nav-item active">
+        <a class="nav-item nav-link" href="/login">Log in</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-item nav-link" href="/register">{{ __('messages.register') }}</a>
+      </li>
+    </ul>
+    @endif
+    
+   </div>
 </nav>
-
-<script src="jquery/jquery.js"></script>
-<script type="text/javascript" src='js/bootstrap.min.js'></script>
-<link rel="stylesheet" href="css/bootstrap.css" />
